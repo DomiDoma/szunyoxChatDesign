61947974da9b1528d34096477e6fc570a7bf2540
Croustys <akos.barabas2002@gmail.com>
2022-03-10 19:56:17 +0100

    login user interactions, login implementation

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,13 +1,41 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 
+import { useRef } from "react";
+
 const Login: NextPage = () => {
+  const emailRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+
+  const handleSubmit = async () => {
+    const email = emailRef.current?.value;
+    const password = passwordRef.current?.value;
+
+    /* login fetch */
+
+    /* handle unsuccessful login */
+
+    /* redirect on successful login */
+    const { push } = (await import("next/router")).default;
+    push("/setup");
+  };
+
   return (
     <div className="fixed">
       <form id="login" action="/setup">
         <h3>Log in to your SzunyoX account!</h3>
-        <input id="username" type="email" placeholder="Email address" />
-        <input id="password" type="password" placeholder="Password" />
+        <input
+          id="username"
+          type="email"
+          placeholder="Email address"
+          ref={emailRef}
+        />
+        <input
+          id="password"
+          type="password"
+          placeholder="Password"
+          ref={passwordRef}
+        />
         <p>or</p>
         <button className="appleBtn">
           <img src="../logo/apple.png" alt="" />
@@ -17,7 +45,9 @@ const Login: NextPage = () => {
           <img src="../logo/google.png" alt="" />
           <span>Sign in with Google</span>
         </button>
-        <button className="mainBtn">Start Chatting</button>
+        <button className="mainBtn" type="button" onClick={handleSubmit}>
+          Start Chatting
+        </button>
         <a className="margin" href="#">
           Forgot password
         </a>
