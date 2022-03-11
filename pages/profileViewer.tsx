5435b001556eae6fac47d3c4652ed9858ed0508a
Croustys <akos.barabas2002@gmail.com>
2022-03-11 17:23:30 +0100

    scrollposition based img width, needs fix

--- pages/profileViewer.tsx
+++ pages/profileViewer.tsx
@@ -2,15 +2,29 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useEffect, useRef } from "react";
+
+import useScrollPosition from "@hooks/useScrollPosition";
 
 import Layout from "@components/Layout";
 
-
 const ProfileViewer: NextPage = () => {
   const { push } = useRouter();
+  const scrollPosition = useScrollPosition();
+  const pfpRef = useRef<HTMLDivElement>(null);
+
+  //@FIXME: width change based on scroll position
+  useEffect(() => {
+    if (scrollPosition === 0) {
+      //@ts-ignore
+      pfpRef.current?.style.width = `$100%`;
+    }
+    //@ts-ignore
+    pfpRef.current?.style.width = `${100 - scrollPosition}%`;
+  }, [scrollPosition]);
   return (
     <Layout>
-        <header>
+      <header>
         <button title="Back" className="header-btn" onClick={() => push("/")}>
           <img
             alt=""
@@ -25,50 +39,65 @@ const ProfileViewer: NextPage = () => {
           />
         </button>
       </header>
-        <section className="profileHeader">
+      <section ref={pfpRef} className="profileHeader">
         <img
-              id="profile-img"
-              src="https://media.karousell.com/media/photos/products/2022/2/8/big_floppa_1644303126_391dfcc8_progressive.jpg"
-              alt=""
-            />
-            <div className="row">
-                <h2 id="name">Big Floppa</h2>
-                <h2 id="age">23</h2>
-            </div>
-        </section>
+          id="profile-img"
+          src="https://media.karousell.com/media/photos/products/2022/2/8/big_floppa_1644303126_391dfcc8_progressive.jpg"
+          alt=""
+        />
+        <div className="row">
+          <h2 id="name">Big Floppa</h2>
+          <h2 id="age">23</h2>
+        </div>
+      </section>
       <main className="xlmargin">
         <section className="profileData">
-        <div className="tags">
-              <a href="" className="tag">
-                Cement
-              </a>
-              <a href="" className="tag">
-                Friday
-              </a>
-              <a href="" className="tag">
-                Funny Ears
-              </a>
-            </div>
-            <div className="bio">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus dolor atque voluptas possimus! Ex ipsam, possimus impedit, facilis veritatis dolore sint unde quas, assumenda reiciendis beatae pariatur enim expedita deserunt adipisci. Beatae voluptatem est ea dolorem laboriosam repellendus voluptate ad.
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, ut facere. Reiciendis libero in tenetur nisi aliquid quo odio a consectetur optio, assumenda repudiandae et id eaque animi neque dolorem?
-            </div>
-            <div className="tags social">
-            <a href="https://www.instagram.com/prozhony/" target="_blank" className="tag">
-              <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2Fe%2Fe7%2FInstagram_logo_2016.svg%2F1200px-Instagram_logo_2016.svg.png&f=1&nofb=1" alt="" />
-                @prozhony
-              </a>
+          <div className="tags">
             <a href="" className="tag">
-              <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Flogo-logos.com%2Fwp-content%2Fuploads%2F2016%2F11%2FTelegram_logo.png&f=1&nofb=1" alt="" />
-                @flopper
-              </a>
-            </div>
+              Cement
+            </a>
+            <a href="" className="tag">
+              Friday
+            </a>
+            <a href="" className="tag">
+              Funny Ears
+            </a>
+          </div>
+          <div className="bio">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus
+            dolor atque voluptas possimus! Ex ipsam, possimus impedit, facilis
+            veritatis dolore sint unde quas, assumenda reiciendis beatae
+            pariatur enim expedita deserunt adipisci. Beatae voluptatem est ea
+            dolorem laboriosam repellendus voluptate ad. Lorem ipsum dolor sit
+            amet consectetur adipisicing elit. Id, ut facere. Reiciendis libero
+            in tenetur nisi aliquid quo odio a consectetur optio, assumenda
+            repudiandae et id eaque animi neque dolorem?
+          </div>
+          <div className="tags social">
+            <a
+              href="https://www.instagram.com/prozhony/"
+              target="_blank"
+              className="tag"
+              rel="noopener noreferrer"
+            >
+              <img
+                src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2Fe%2Fe7%2FInstagram_logo_2016.svg%2F1200px-Instagram_logo_2016.svg.png&f=1&nofb=1"
+                alt=""
+              />
+              @prozhony
+            </a>
+            <a href="" className="tag">
+              <img
+                src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Flogo-logos.com%2Fwp-content%2Fuploads%2F2016%2F11%2FTelegram_logo.png&f=1&nofb=1"
+                alt=""
+              />
+              @flopper
+            </a>
+          </div>
         </section>
-        <script src="../js/sticky.js"></script>
       </main>
     </Layout>
   );
 };
-
 
 export default ProfileViewer;
